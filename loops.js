e35d3eb13487aaf5d74c408948e310af0e91aae2
qaisaru@icloud.com <qaisaru@icloud.com>
2018-07-10 13:06:17 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array){
   for (let i=0; i<25; i++){
-    if(i===1){console.log(`I am 1 strange loop.`)}
-    else {console.log(`I am ${i} strange loops.`)}
+    if(i===1){`I am 1 strange loop.`}
+    else {`I am ${i} strange loops.`)}
   }
 }
